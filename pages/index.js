05871bff70640a8dd361fab7884518f07639e1be
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-08-18 01:34:59 -0300

    update counter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head'
-import CounterNumber from '../component/counter'
+import { useRouter } from 'next/router'
+import { connectToDatabase } from '../config/mongodb'
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/index.module.css'
 
-export default function Home() {
+export default function Home({ sequence }) {
+  const router = useRouter()
+  const { service } = router.query
+  const number = sequence.toString().padStart(6, "0")
+  const counter = [number.slice(0, 3), '.', number.slice(3)].join('')
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +18,25 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <CounterNumber />
+          {counter}
         </h1>
       </main>
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const { db, client } = await connectToDatabase()
+  const data = await db.collection('visitorcounter-col').findOneAndUpdate(
+    { service: "xxx" },
+    { $inc: { sequence: 1 } }
+  )
+  const { sequence } = await db.collection('visitorcounter-col').findOne({ 'service': 'xxx' })
+
+  return {
+    props: {
+      sequence: '' + sequence
+    },
+  }
+}
+
